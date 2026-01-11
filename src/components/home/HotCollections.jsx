f0a3e947194a58d260d5c4506f31317d364baa4c
iarocci44-byte <iarocci44@gmail.com>
2026-01-11 15:59:09 -0500

    implemented hot collections dynamic

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const HotCollections = () => {
+  const [collections, setCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const sliderRef = React.useRef(null);
+
+  useEffect(() => {
+    const fetchCollections = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+        console.log(response.data);
+        setCollections(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching hot collections:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchCollections();
+  }, []);
+
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  };
+
   return (
-    <section id="section-collections" className="no-bottom">
+    <section id="section-collections" className="no-bottom section-collections">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -14,30 +71,106 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
+        </div>
+
+        {loading ? (
+          <div className="row">
+            {new Array(4).fill(0).map((_, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <div className="skeleton skeleton-image"></div>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <div className="skeleton skeleton-avatar"></div>
+                  </div>
+                  <div className="nft_coll_info">
+                    <div className="skeleton skeleton-title"></div>
+                    <div className="skeleton skeleton-text"></div>
+                  </div>
                 </div>
               </div>
+            ))}
+          </div>
+        ) : (
+          <div className="row">
+            <div className="col-lg-12">
+              <div style={{ position: 'relative' }}>
+                <Slider ref={sliderRef} {...sliderSettings}>
+                  {collections.map((collection, index) => (
+                    <div key={index}>
+                      <div className="nft_coll">
+                        <div className="nft_wrap">
+                          <Link to="/item-details">
+                            <img src={collection.nftImage || nftImage} className="lazy img-fluid" alt="" />
+                          </Link>
+                        </div>
+                        <div className="nft_coll_pp">
+                          <Link to="/author">
+                            <img className="lazy pp-coll" src={collection.authorImage || AuthorImage} alt="" />
+                          </Link>
+                          <i className="fa fa-check"></i>
+                        </div>
+                        <div className="nft_coll_info">
+                          <Link to="/explore">
+                            <h4>{collection.title || "Pinky Ocean"}</h4>
+                          </Link>
+                          <span>{collection.code || "ERC-192"}</span>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </Slider>
+                <button
+                  onClick={() => sliderRef.current.slickPrev()}
+                  style={{
+                    position: 'absolute',
+                    left: '-10px',
+                    top: '50%',
+                    transform: 'translateY(-50%)',
+                    background: '#fff',
+                    border: '2px solid #333',
+                    borderRadius: '50%',
+                    width: '50px',
+                    height: '50px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    fontSize: '24px',
+                    cursor: 'pointer',
+                    zIndex: 10,
+                    color: '#333'
+                  }}
+                >
+                  <i className="fa fa-chevron-left"></i>
+                </button>
+                <button
+                  onClick={() => sliderRef.current.slickNext()}
+                  style={{
+                    position: 'absolute',
+                    right: '-10px',
+                    top: '50%',
+                    transform: 'translateY(-50%)',
+                    background: '#fff',
+                    border: '2px solid #333',
+                    borderRadius: '50%',
+                    width: '50px',
+                    height: '50px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    fontSize: '24px',
+                    cursor: 'pointer',
+                    zIndex: 10,
+                    color: '#333'
+                  }}
+                >
+                  <i className="fa fa-chevron-right"></i>
+                </button>
+              </div>
             </div>
-          ))}
-        </div>
+          </div>
+        )}
       </div>
     </section>
   );
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,13 +1,96 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
 import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 import nftImage from "../images/nftImage.jpg";
+import axios from 'axios';
+
 
 const ItemDetails = () => {
+  const [itemData, setItemData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    
+    // Fetch data from API
+    const fetchItemData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+        console.log(response.data);
+        
+        // Get first item from API response
+        const item = response.data[0];
+        
+        // Transform API data to match your component structure
+        const transformedData = {
+          title: item.title || "Rainbow Style #194",
+          description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+          views: Math.floor(Math.random() * 500),
+          likes: item.likes || Math.floor(Math.random() * 200),
+          price: item.price || (Math.random() * 5 + 0.5).toFixed(2),
+          owner: {
+            name: item.authorName || "Monica Lucas",
+            image: item.authorImage || AuthorImage
+          },
+          creator: {
+            name: item.authorName || "Monica Lucas",
+            image: item.authorImage || AuthorImage
+          },
+          nftImage: item.nftImage || nftImage
+        };
+        
+        setItemData(transformedData);
+        setLoading(false);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchItemData();
   }, []);
+
+  if (loading) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>
+          <section aria-label="section" className="mt90 sm-mt-0">
+            <div className="container">
+              <div className="row">
+                <div className="col-12 text-center">
+                  <h3>Loading...</h3>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>
+          <section aria-label="section" className="mt90 sm-mt-0">
+            <div className="container">
+              <div className="row">
+                <div className="col-12 text-center">
+                  <h3>Error: {error}</h3>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div id="wrapper">
@@ -18,29 +101,27 @@ const ItemDetails = () => {
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={itemData?.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
                   alt=""
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>{itemData?.title}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {itemData?.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {itemData?.likes}
                     </div>
                   </div>
                   <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
+                    {itemData?.description}
                   </p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
@@ -48,12 +129,12 @@ const ItemDetails = () => {
                       <div className="item_author">
                         <div className="author_list_pp">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img className="lazy" src={itemData?.owner.image} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to="/author">{itemData?.owner.name}</Link>
                         </div>
                       </div>
                     </div>
@@ -65,12 +146,12 @@ const ItemDetails = () => {
                       <div className="item_author">
                         <div className="author_list_pp">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img className="lazy" src={itemData?.creator.image} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to="/author">{itemData?.creator.name}</Link>
                         </div>
                       </div>
                     </div>
@@ -78,7 +159,7 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>{itemData?.price}</span>
                     </div>
                   </div>
                 </div>
