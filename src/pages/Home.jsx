48d476180a7e4b0d3f478611cd4891e6b5f72a81
iarocci44-byte <iarocci44@gmail.com>
2026-01-19 14:31:48 -0500

    installed AOS animations and implemented on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import the AOS CSS file
+
 
 const Home = () => {
   useEffect(() => {
